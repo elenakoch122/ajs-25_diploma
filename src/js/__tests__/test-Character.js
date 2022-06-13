992540676df678dfce9276b27982cd9228827f2d
Elena Kochegarova <lelet@ngs.ru>
2022-06-13 19:36:30 +0700

    added tests

--- src/js/__tests__/test-Character.js
+++ src/js/__tests__/test-Character.js
@@ -1,5 +1,5 @@
 import Character from '../Character';
-import Bowman from '../Bowman';
+import Bowman from '../characters/Bowman';
 
 test('preventing the creation of Character objects', () => {
   expect(() => new Character(1, 'bowman')).toThrow(new Error('Нельзя создавать объекты через new Character()'));
@@ -7,4 +7,38 @@ test('preventing the creation of Character objects', () => {
 
 test('create an instance of the inherited class, for example Bowman', () => {
   expect(new Bowman(1)).toBeInstanceOf(Bowman);
+});
+
+test('levelUp method, should raise level by 1', () => {
+  const char = new Bowman(1);
+  char.levelUp();
+  expect(char.level).toBe(2);
+});
+
+test('levelUp method, health is increased by 80', () => {
+  const char = new Bowman(1);
+  char.health = 15;
+  char.levelUp();
+  expect(char.health).toBe(95);
+});
+
+test('levelUp method, health cannot be more than 100', () => {
+  const char = new Bowman(1);
+  char.health = 30;
+  char.levelUp();
+  expect(char.health).toBe(100);
+});
+
+test('levelUp method, calculates the attack', () => {
+  const char = new Bowman(1);
+  char.health = 30;
+  char.levelUp();
+  expect(char.attack).toBe(27);
+});
+
+test('levelUp method, calculates the defence', () => {
+  const char = new Bowman(1);
+  char.health = 10;
+  char.levelUp();
+  expect(char.attack).toBe(25);
 });
