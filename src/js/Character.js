649a807f81208371ef08ed5a0429d3b019f84aba
Elena Kochegarova <lelet@ngs.ru>
2022-06-13 18:48:03 +0700

    completed levelUp method

--- src/js/Character.js
+++ src/js/Character.js
@@ -13,10 +13,14 @@ export default class Character {
 
   levelUp() {
     this.level += 1;
+
+    const life = (1.8 - (1 - this.health / 100));
+    this.attack = Math.floor(Math.max(this.attack, this.attack * life));
+    this.defence = Math.floor(Math.max(this.defence, this.defence * life));
+
     this.health += 80;
     if (this.health > 100) {
       this.health = 100;
     }
-    // attackAfter = Math.max(attackBefore, attackBefore * (1.8 - life) / 100) прописать код
   }
 }
